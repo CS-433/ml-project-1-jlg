479cdd344ea3216ee36ea7b358210e0f25152b01
Louis-Alexandre-Ongaro <ongaro.louisalexandre@gmail.com>
2021-10-12 17:03:36 +0200

    Changes in implementation and helpers2
    - batch size
    - GD
    - SGD

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -3,10 +3,10 @@
 
 from helpers2 import *
 
-def gradient_descent(y, tx, initial_w, max_iters, gamma):
-    """Gradient descent algorithm."""
-    ws = [initial_w]
-    losses = []
+
+def least squares GD(y, tx, initial_w, max_iters, gamma):
+    """Linear regression using gradient descent"""
+    # GD donne loss et w 
     w = initial_w
     for n_iter in range(max_iters):
         # compute gradient and loss
@@ -14,40 +14,22 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
         loss = compute_loss(y, tx, w)
         # update w by gradient
         w = w - gamma*grad
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
-        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1 {w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-    return losses, ws
-
-def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma):
-    """Stochastic gradient descent algorithm.""".
-    ws = [initial_w]
-    losses = []
-    w = initial_w
-    for n_iter in range(max_iters):
-        for minibatch_y, minibatch_tx in batch_iter(y, tx, 1):
-            G = compute_stoch_gradient(minibatch_y, minibatch_tx, w)
-            loss = compute_loss(y, tx, w)
-            w = w - gamma*G
-
-        ws.append(w)
-        losses.append(loss)
-        
-        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-    return losses, ws
-
-
-def least squares GD(y, tx, initial w, max_iters, gamma):
-    """Linear regression using gradient descent"""
-    # GD donne loss et w 
     return w, loss
 
 
-def least squares SGD(y, tx, initial w, max_iters, gamma):
+def least squares SGD(y, tx, initial_w, max_iters, gamma):
     """Linear regression using stochastic gradient descent"""
+    w = initial_w
+    for n_iter in range(max_iters):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx):
+            G = compute_stoch_gradient(minibatch_y, minibatch_tx, w)
+            loss = compute_loss(y, tx, w)
+            w = w - gamma*G
+        
+        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
     return w, loss
 
 
@@ -71,12 +53,12 @@ def ridge regression(y, tx, lambda_):
     return w, loss
 
 
-def logistic regression(y, tx, initial w, max iters, gamma):
+def logistic regression(y, tx, initial_w, max iters, gamma):
     """Logistic regression using gradient descent or SGD"""
     return w, loss
 
 
-def reg logistic regression(y, tx, lambda_, initial w, max iters, gamma):
+def reg logistic regression(y, tx, lambda_, initial_w, max_iters, gamma):
     """Logistic regression using gradient descent or SGD"""
     return w, loss
 
