21ef0e5ec743e6e9c90077ae037c5c526471b27b
ghaeflig <56826275+ghaeflig@users.noreply.github.com>
2021-10-14 15:10:26 +0200

    essais de quelques méthodes et visualisation des data

--- scripts/helpers2.py
+++ scripts/helpers2.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-def compute_mse_loss(y, tx, w):
+def compute_mse(y, tx, w):
     """Calculate the mse for error vector e."""
     e = y - tx.dot(w)
     return 1/2*np.mean(e**2)
@@ -13,7 +13,7 @@ def compute_gradient(y, tx, w):
     """Compute the gradient."""
     e = y - tx.dot(w)
     G = -tx.T.dot(e) / len(e)
-    return G, e
+    return G
 
 
 def batch_iter(y, tx, batch_size=1, num_batches=1, shuffle=True):
--- scripts/implementations.py
+++ scripts/implementations.py
@@ -12,7 +12,7 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     for n_iter in range(max_iters):
         # compute gradient and loss
         grad = compute_gradient(y, tx, w)
-        loss = compute_loss(y, tx, w)
+        loss = compute_mse(y, tx, w)
         # update w by gradient
         w = w - gamma*grad
         print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1 {w1}".format(
@@ -25,10 +25,11 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     w = initial_w
     for n_iter in range(max_iters):
         for minibatch_y, minibatch_tx in batch_iter(y, tx):
-            G = compute_stoch_gradient(minibatch_y, minibatch_tx, w)
-            loss = compute_loss(y, tx, w)
+            # compute gradient and loss
+            G = compute_gradient(minibatch_y, minibatch_tx, w)
+            loss = compute_mse(y, tx, w)
+            # update w by gradient
             w = w - gamma*G
-        
         print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
     return w, loss
@@ -36,25 +37,21 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
 
 def least_squares(y, tx):
     """Least squares regression using normal equations"""
-    w_star = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
-    loss = compute_mse(y, tx, w_star)
+    w = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
+    loss = compute_mse(y, tx, w)
     return w, loss
 
 
 def ridge_regression(y, tx, lambda_):
     """Ridge regression using normal equations"""
-    N = tx.shape[1]
-    lamb = 2*N*lambda_*np.identity(N)
-    a = tx.T.dot(tx) + lamb
-    b = tx.T.dot(y)
-    w = np.linalg.solve(a, b)
-    
-    e = y - tx.dot(w)
-    loss = e.dot(e) / (2 * len(e))
+    lambda_2 = lambda_*2*tx.shape[0]
+    I = np.identity(tx.shape[1])
+    w = np.linalg.solve(tx.T.dot(tx) + I.dot(lambda_2), tx.T.dot(y))
+    loss = compute_mse(y, tx, w)
     return w, loss
 
 
-def logistic_regression(y, tx, initial_w, max iters, gamma):
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
     """Logistic regression using gradient descent or SGD"""
     return w, loss
 
