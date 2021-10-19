789e0af3e98ee59db1eb0f3a5b7163d95bfa4c7c
julia-walti <91602293+julia-walti@users.noreply.github.com>
2021-10-19 18:31:48 +0200

    Add files via upload

--- scripts/helpers2.py
+++ scripts/helpers2.py
@@ -3,6 +3,32 @@
 
 import numpy as np
 
+def split_data(x, y, ids, ratio, seed=1):
+    """split the dataset based on the split ratio."""
+    # set seed
+    np.random.seed(seed)
+    indices = np.arange(x.shape[0])
+    np.random.shuffle(indices)
+
+    n          = x.shape[0]
+    index_split = int(n*ratio)
+    x_train    = x[indices[0:index_split]]
+    y_train    = y[indices[0:index_split]] 
+    ids_train = ids[indices[0:index_split]]
+    x_test     = x[indices[index_split:]]
+    y_test     = y[indices[index_split:]] 
+    ids_test = ids[indices[index_split:]] 
+    
+    return x_train, y_train, x_test, y_test, ids_train, ids_test
+
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+
 def compute_mse(y, tx, w):
     """Calculate the mse for error vector e."""
     e = y - tx.dot(w)
@@ -40,3 +66,10 @@ def batch_iter(y, tx, batch_size=1, num_batches=1, shuffle=True):
         end_index = min((batch_num + 1) * batch_size, data_size)
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+            
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    poly = np.ones((x.shape[0], 1))
+    for deg in range(1, degree+1):
+        poly = np.c_[poly, np.power(x, deg)]
+    return poly
--- scripts/implementations.py
+++ scripts/implementations.py
@@ -4,37 +4,6 @@
 import numpy as np
 from helpers2 import *
 
-
-def least_squares_GD(y, tx, initial_w, max_iters, gamma):
-    """Linear regression using gradient descent"""
-    # GD donne loss et w 
-    w = initial_w
-    for n_iter in range(max_iters):
-        # compute gradient and loss
-        grad = compute_gradient(y, tx, w)
-        loss = compute_mse(y, tx, w)
-        # update w by gradient
-        w = w - gamma*grad
-        print("Gradient Descent({bi}/{ti}): loss={l}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss))
-    return w, loss
-
-
-def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
-    """Linear regression using stochastic gradient descent"""
-    w = initial_w
-    for n_iter in range(max_iters):
-        for minibatch_y, minibatch_tx in batch_iter(y, tx):
-            # compute gradient and loss
-            G = compute_gradient(minibatch_y, minibatch_tx, w)
-            loss = compute_mse(y, tx, w)
-            # update w by gradient
-            w = w - gamma*G
-        print("SGD({bi}/{ti}): loss={l}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss))
-    return w, loss
-
-
 def least_squares(y, tx):
     """Least squares regression using normal equations"""
     w = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
@@ -48,6 +17,34 @@ def ridge_regression(y, tx, lambda_):
     I = np.identity(tx.shape[1])
     w = np.linalg.solve(tx.T.dot(tx) + I.dot(lambda_2), tx.T.dot(y))
     loss = compute_mse(y, tx, w)
+    return w, loss
+
+def least_squares_GD(y, tx, gamma, max_iters=50):
+    """Linear regression using gradient descent"""
+    # GD donne loss et w
+    w=np.zeros(tx.shape[1])
+    for n_iter in range(max_iters):
+        # compute gradient and loss
+        grad = compute_gradient(y, tx, w)
+        loss = compute_mse(y, tx, w)
+        # update w by gradient
+        w = w - gamma*grad
+        print("Gradient Descent({bi}/{ti}): loss={l}".format(bi=n_iter, ti=max_iters - 1, l=loss))
+    return w, loss
+
+
+def least_squares_SGD(y, tx,  gamma, max_iters=50):
+    """Linear regression using stochastic gradient descent"""
+    w=np.zeros(tx.shape[1])
+    for n_iter in range(max_iters):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx):
+            # compute gradient and loss
+            G = compute_gradient(minibatch_y, minibatch_tx, w)
+            loss = compute_mse(y, tx, w)
+            # update w by gradient
+            w = w - gamma*G
+        print("SGD({bi}/{ti}): loss={l}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss))
     return w, loss
 
 
