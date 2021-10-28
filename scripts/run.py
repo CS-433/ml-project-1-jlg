dd57e1d61be836501df4c84a00690a4a03b5ea0e
julia-walti <91602293+julia-walti@users.noreply.github.com>
2021-10-28 09:17:06 +0200

    Add files via upload

--- scripts/run.py
+++ scripts/run.py
@@ -8,12 +8,13 @@ from helpers2 import *
 from preprocessing import * 
 
 #### DOWNLOAD DATA ####
-
+DATA_TRAIN_PATH = "../data/train.csv"
+y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
 
 #### PREPROCESSING ####
 
 
-#### MODELS APPLICATION #####
+#### BEST MODEL APPLICATION #####
 
 #### Least squares ####
 
@@ -30,7 +31,9 @@ from preprocessing import *
 
 
 #### CREATE SUBMISSION ####
+DATA_TEST_PATH = '../data/test.csv' # TODO: download train data and supply path here 
+_, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
 
 
-
-
+OUTPUT_PATH = '../data/best_pred_JLG.csv'
+create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
